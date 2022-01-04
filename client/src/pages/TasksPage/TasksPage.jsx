c5ef55fbda327cfea10d80555f541cadedb079e1
PDOUU <58252790+pdouu@users.noreply.github.com>
2022-01-05 00:39:15 +0300

    fix: change tab wrong tab name

--- client/src/pages/TasksPage/TasksPage.jsx
+++ client/src/pages/TasksPage/TasksPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
+import { Tab } from '@headlessui/react';
 import Modal from 'components/Modal/Modal';
 import ActiveTable from './components/ActiveTable';
 import HistoryTable from './components/HistoryTable';
 import AddTask from './components/AddTask';
 import EditTask from './components/EditTask';
 
-import { Tab } from '@headlessui/react';
 import { ClipboardCheckIcon, ClipboardListIcon } from '@heroicons/react/solid';
 
 export default function TasksPage() {
@@ -27,7 +27,7 @@ export default function TasksPage() {
       >
         <Tab.List className="flex gap-x-2 justify-center">
           <Tab
-            className="basis-1/2 md:basis-2/12 lg:basis-48 2xl:basis-68 active:translate-y-1 inline-flex w-full px-5 justify-center gap-x-5 items-center text-sm font-semibold border border-transparent shadow-lg rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 h-12 duration-75 bg-white h-fullmy-2 text-indigo-500 hover:bg-indigo-600 hover:text-white"
+            className="basis-1/2 md:basis-2/12 lg:basis-48 2xl:basis-72 active:translate-y-1 inline-flex w-full px-5 justify-center gap-x-5 items-center text-sm font-semibold border border-transparent shadow-lg rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 h-12 duration-75 bg-white h-fullmy-2 text-indigo-500 hover:bg-indigo-600 hover:text-white"
             style={{
               backgroundColor: !selectedTabIndex ? '#4f46e5' : 'white',
               color: !selectedTabIndex ? 'white' : '#6366f1',
@@ -37,14 +37,14 @@ export default function TasksPage() {
             <span>Aktif Görevler</span>
           </Tab>
           <Tab
-            className="basis-1/2 md:basis-2/12 lg:basis-48 2xl:basis-68 active:translate-y-1 inline-flex w-full px-5 justify-center gap-x-5 items-center text-sm font-semibold border border-transparent shadow-lg rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 h-12 duration-75 bg-white h-fullmy-2 text-indigo-500 hover:bg-indigo-600 hover:text-white"
+            className="basis-1/2 md:basis-2/12 lg:basis-48 2xl:basis-72 active:translate-y-1 inline-flex w-full px-5 justify-center gap-x-5 items-center text-sm font-semibold border border-transparent shadow-lg rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 h-12 duration-75 bg-white h-fullmy-2 text-indigo-500 hover:bg-indigo-600 hover:text-white"
             style={{
               backgroundColor: selectedTabIndex ? '#4f46e5' : 'white',
               color: selectedTabIndex ? 'white' : '#6366f1',
             }}
           >
             <ClipboardCheckIcon className="w-6" />
-            <span>Aktif Görevler</span>
+            <span>Tamamlanmış Görevler</span>
           </Tab>
         </Tab.List>
         <Tab.Panels>
