be6dd851da645b9656ae44cb6b52bb38435c214f
PDOUU <58252790+pdouu@users.noreply.github.com>
2022-01-05 21:28:51 +0300

    feat: Get only changed inputs on task and user forms

---
+++ client/src/utils/utils.js
@@ -0,0 +1,10 @@
+export const getOnlyChangedInputs = (data, initialValues) => {
+  const modifiedData = Object.keys(data).reduce((prevObj, currentKey) => {
+    if (data[currentKey] !== initialValues[currentKey]) {
+      return { ...prevObj, [currentKey]: data[currentKey] };
+    } else return { ...prevObj };
+  }, {});
+  if (Object.keys(modifiedData).length) {
+    return { id: initialValues.id, ...modifiedData };
+  } else return undefined;
+};
