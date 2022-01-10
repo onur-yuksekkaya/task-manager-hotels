06cb5b4ba298b03f62c81872e46b3339608724ed
PDOUU <58252790+pdouu@users.noreply.github.com>
2022-01-10 13:47:38 +0300

    refactor: page layout

--- client/src/components/Buttons/SidebarButton.jsx
+++ client/src/components/Buttons/SidebarButton.jsx
@@ -3,7 +3,7 @@ import { QuestionMarkCircleIcon } from '@heroicons/react/outline';
 
 export default function SidebarButton({
   text = 'Button',
-  customStyleClass = 'bg-yellow-500 text-black',
+  customStyleClass = '',
   type = 'button',
   onClick = () => {},
   icon = <QuestionMarkCircleIcon className="w-6 ml-5" />,
@@ -11,7 +11,7 @@ export default function SidebarButton({
   return (
     <button
       type={type}
-      className={`inline-flex w-16 h-14 lg:h-12 lg:w-10/12 text-xs md:text-sm 2xl:text-sm justify-center items-center lg:justify-start lg:gap-5 flex-col lg:flex-row font-light rounded-xl lg:rounded-md focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 active:translate-y-1 duration-150 lg:shadow-lg ${customStyleClass} `}
+      className={`inline-flex w-16 h-14 lg:h-12 lg:w-11/12 text-xs md:text-sm 2xl:text-sm justify-center items-center lg:justify-start lg:gap-5 flex-col lg:flex-row font-light rounded-xl lg:rounded-md focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 active:translate-y-1 duration-150 lg:font-semibold lg:bg-indigo-200 hover:bg-indigo-700 hover:text-white text-indigo-700 ${customStyleClass} `}
       onClick={onClick}
     >
       {icon}
--- client/src/components/Headings/HeadingSubtitle.jsx
+++ client/src/components/Headings/HeadingSubtitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function HeadingSubtitle({ text, customClass = 'text-black' }) {
   return (
-    <h2 className={`text-md 2xl:text-xl font-semibold py-4 ${customClass}`}>
+    <h2 className={`text-md 2xl:text-lg font-semibold py-4 ${customClass}`}>
       {text}
     </h2>
   );
